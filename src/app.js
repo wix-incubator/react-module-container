9fa913072885545f2ffd5c2cccf46ec7b3eb7255
Shahar Talmi <shahart@wix.com>
2016-09-20 01:01:34 +0300

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,16 +1,2 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import Root from './components/Root/Root';
-import counter from './reducers';
-
-const store = createStore(counter);
-const rootEl = document.getElementById('root');
-
-render(
-  <Provider store={store}>
-    <Root/>
-  </Provider>,
-  rootEl
-);
+console.log('hiii');
+document.write('<p>0</p>');
--- test/mock/fake-server.js
+++ test/mock/fake-server.js
@@ -3,17 +3,9 @@
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
-import session from 'express-session';
-import {renderVM} from './vm';
 
 const app = express();
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'yoba',
-  cookie: {httpOnly: false},
-  resave: false,
-  saveUninitialized: true
-}));
 
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
@@ -22,29 +14,8 @@ app.use((req, res, next) => {
   return next();
 });
 
-const storage = {};
-
-function getTodos(req) {
-  storage[req.sessionID] = storage[req.sessionID] || [];
-  return storage[req.sessionID];
-}
-
-function setTodos(req, todos) {
-  storage[req.sessionID] = todos;
-}
-
-app.route('/_api/todo-list-server/todos')
-  .get((req, res) => {
-    res.send({todos: getTodos(req)});
-  })
-  .post((req, res) => {
-    req.body.id = Math.random().toString();
-    setTodos(req, getTodos(req).concat([req.body]));
-    res.send(req.body);
-  });
-
 app.use('*', (req, res) => {
-  res.send(renderVM(path.join(process.cwd(), './src/index.vm')));
+  res.sendFile(path.join(process.cwd(), './src/index.html'));
 });
 
 const port = process.env.FAKE_SERVER_PORT || 3000;
