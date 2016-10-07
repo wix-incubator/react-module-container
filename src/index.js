a40bb6d5fa723e7ddbfcfb30951c6aa30399cae9
Shahar Talmi <shahart@wix.com>
2016-10-07 21:09:52 +0300

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import ModuleRegistry from './module-registry';
-import ReactLazyComponent from './react-lazy-component';
-import AngularLazyComponent from './angular-lazy-component';
-
-export default {ModuleRegistry, ReactLazyComponent, AngularLazyComponent};
+export {default as ModuleRegistry} from './module-registry';
+export {default as ReactLazyComponent} from './react-lazy-component';
+export {default as AngularLazyComponent} from './angular-lazy-component';
