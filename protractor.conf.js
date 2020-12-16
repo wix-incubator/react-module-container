1e91ab0fd4070b2e885015fdbf004f6ef1276a3b
Ronen Stern <ronens@wix.com>
2020-12-16 10:54:10 +0200

    Upgrade Yoshi

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 process.env.FAKE_SERVER_PORT = 3100;
 
 module.exports.config = {
-  specs: ['dist/test/**/*.e2e.js'],
+  // specs: ['dist/test/**/*.e2e.js'],
   baseUrl: `http://localhost:${process.env.FAKE_SERVER_PORT}/`,
 
   onPrepare() {
