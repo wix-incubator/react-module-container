78ee7228a646e2f9a5aeaa33fbbe3a71f940fb39
Ronen Stern <ronens@wix.com>
2021-10-03 19:47:36 +0300

    Add `ReactModuleContainerContext.displayName` for debugging

--- src/context.js
+++ src/context.js
@@ -2,3 +2,4 @@ import { createContext } from 'react';
 
 /** @deprecated `ReactModuleContainerContext` exists as a workaround while transitioning from legacy features, please do not use */
 export const ReactModuleContainerContext = createContext(null);
+ReactModuleContainerContext.displayName = 'ReactModuleContainerContext(deprecated)';
